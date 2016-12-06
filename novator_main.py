8121212342fff7602dc407dfe808dd742ba79f46
Artem Borovik <borovikao88@gmail.com>
2016-12-06 19:09:01 +0600

    New version 20161206

--- novator_main.py
+++ novator_main.py
@@ -1,13 +1,10 @@
-import sys
-from ftplib import FTP
+import argparse
+from sys import argv
+from ftplib import FTP, all_errors
 from Lib import tempfile
-from os.path import isfile
-from os.path import exists
-from os.path import join as joinpath
-from os import mkdir
-from os import listdir
-from os import remove
-from src.ftp.ftp_connector import FtpConnector
+from os.path import isfile, exists, join as joinpath
+from os import mkdir, listdir, remove, walk
+
 
 
 #temppath = tempfile.gettempdir()+'\orders'
@@ -17,6 +14,51 @@ temppath = "E:\\upload\\НоваТор"
 DIR_NEW_ORDERS = (temppath+'\orders')
 # Directory of new submission
 DIR_NEW_ORDRSP = (temppath+'\submission')
+# Host FTP server
+HOST = ''
+# Login FTP server
+LOGIN = ''
+# Password from login
+PASSW = ''
+# Timeout for connection FTP
+TIMEOUT = 0
+
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--server')
+    parser.add_argument('-l', '--login')
+    parser.add_argument('-p', '--passw')
+    parser.add_argument('-t', '--timeout', default=0)
+
+    return parser
+
+
+def get_orders(ftp):
+    _cwd = '/ORDERSLOADED'
+    if ftp.pwd() != _cwd:
+        ftp.cwd(_cwd)
+
+    #ftp.retrlines('LIST')
+
+    count = 5
+    for files in walk(_cwd):
+        for file in files:
+            full_name = joinpath(_cwd, file)
+            # Name files on FTP
+            fname = str(file)
+            # Create local file
+            localfile = open(DIR_NEW_ORDRSP+'\%s' % fname, 'wb')
+            # Download and write ftp-file to local file
+            ftp.retrbinary('RETR %s' % fname, localfile.write())
+
+            #Close localfile
+            localfile.close()
+            count -= 1
+
+        if count == 0:
+            break
+
 
 if not exists(DIR_NEW_ORDERS):
     # Create new directory
@@ -42,10 +84,34 @@ for i in listdir(DIR_NEW_ORDRSP):
     else:
         continue
 
-ftp = FTP("178.74.69.118", "700", "ord-f066da5a-cd0920cf-b11177d0-46f66e92")
-print(ftp.pwd())
+# ++DEBUG
+argv.append('-s 178.74.69.118')
+argv.append('-l 700')
+argv.append('-p ord-f066da5a-cd0920cf-b11177d0-46f66e92')
+# --
 
-#ftp = FtpConnector("178.74.69.118", "700", "ord-f066da5a-cd0920cf-b11177d0-46f66e92")
-#ftp.get_files('ORDERSACCEPT')
+parser = createParser()
+namespace = parser.parse_args(argv[1:])
+
+HOST = namespace.server
+LOGIN = namespace.login
+PASSW = namespace.passw
+TIMEOUT = int(namespace.timeout)
+
+
+if not (HOST or LOGIN or PASSW):
+    # Write log
+    print("Ошибка. Не найдены обязательные параметры запуска (-s, -l, -p)!")
+    raise SystemExit
+
+try:
+    ftp = FTP(HOST.strip(), LOGIN.strip(), PASSW.strip())
+except all_errors as e:
+    # Write log
+    print('ERROR: %s' % e)
+    raise SystemExit
+
+get_orders(ftp)
+ftp.close()
 
 
